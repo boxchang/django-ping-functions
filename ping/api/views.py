959ce06e912996d705162ba08f7304a6fbb8c1e9
box_chang <box_chang@mail.eagleskytech.com>
2019-03-27 17:03:14 +0800

    commit

--- ping/api/views.py
+++ ping/api/views.py
@@ -12,6 +12,7 @@ def subnet_ping(request):
     if request.method == 'POST':
         ip = request.POST.get('ip')
         # result = json.dumps({'127.0.0.1': True})
+        print('web_ip:'+str(ip))
         result = call_subnet_ping(ip)
         return HttpResponse(result, content_type="application/json")
     else:
